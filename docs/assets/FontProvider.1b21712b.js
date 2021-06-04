b0665ad40a482acfec2ba70a16b5f5c2b42f57bc
Haag59 <ran@haag59.com>
2021-06-03 17:23:02 -0700

    local fonts

---
+++ docs/assets/FontProvider.1b21712b.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.745ff060.js";import{F as r}from"./useFontMetrics.7b691c56.js";const o=({children:o,fontManager:t})=>e.createElement(r.Provider,{value:t},o);export{o as F};
--- docs/assets/FontProvider.678dca40.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./vendor.745ff060.js";import{F as r}from"./useFontMetrics.15406ac0.js";const o=({children:o,fontManager:t})=>e.createElement(r.Provider,{value:t},o);export{o as F};
